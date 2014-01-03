a9588e61f949c89fc4e59bc4740f6834f23dfdd5
anehing <anehing@gmail.com>
2014-01-03 16:49:02 +0800

    添加Coupon的dao

---
+++ dao/com/zdqk/laobing/dao/impl/CouponDAOImpl.java
@@ -0,0 +1,19 @@
+package com.zdqk.laobing.dao.impl;
+
+
+
+import org.springframework.stereotype.Component;
+
+import com.zdqk.laobing.dao.CouponDAO;
+import com.zdqk.laobing.dao.UserDAO;
+import com.zdqk.laobing.po.Coupon;
+import com.zdqk.laobing.po.User;
+
+
+
+
+@Component("couponDAO")
+public class CouponDAOImpl extends BasicDaoImpl<Coupon> implements CouponDAO<Coupon>{
+	
+	
+}
